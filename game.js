22bc85be87122752cd38ca13c9620c6ba6f88141
AngelVelasco29 <buymatrix@hotmail.com>
2022-08-23 00:33:58 -0500

    Clase 6 Arreglos multidimensionales en JavaScript

--- game.js
+++ game.js
@@ -1,33 +1,38 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 const startGame = () => {
-    let canvasSize;
+  let canvasSize;
 
-    if(window.innerHeight>window.innerWidth){
-        canvasSize= window.innerWidth *0.8;
-    }else{
-        canvasSize= window.innerHeight *0.8;
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
+  const elementsSize = canvasSize / 10;
+  console.log(canvasSize, elementsSize);
+  game.font = (elementsSize-10) + "px Verdana";
+  game.textAlign = "end";
+
+
+  const mapRows= maps[2];
+  const mapRowCols= mapRows.map(row => row.split(''));
+  console.log(mapRows);
+  console.log(mapRowCols);
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCols[row-1][col-1]], elementsSize * col+5, elementsSize * row-10);
     }
-    canvas.setAttribute('width', canvasSize )
-    canvas.setAttribute('height', canvasSize )
+  }
 
-    const elementsSize= canvasSize / 10;
-    console.log(canvasSize, elementsSize);
-    game.font= elementsSize + 'px Verdana'
-    game.textAlign= 'end';
-
-    for(let ver=1; ver<=10; ver++){
-        for (let hor = 1; hor <=10; hor++) {
-            game.fillText(emojis['X'],elementsSize*hor,elementsSize*ver)
-        }
-    }
-
-    
-    // game.fillRect(0, 0, 100, 100);
-    // game.clearRect(50,50,50,50);
-    // game.font = '25px Verdana'
-    // game.fillStyle = 'purple'
-    // game.textAlign= 'center'
-    // game.fillText('Platzi',0,25)
+  // game.fillRect(0, 0, 100, 100);
+  // game.clearRect(50,50,50,50);
+  // game.font = '25px Verdana'
+  // game.fillStyle = 'purple'
+  // game.textAlign= 'center'
+  // game.fillText('Platzi',0,25)
 };
 window.addEventListener("load", startGame);
--- maps.js
+++ maps.js
@@ -9,39 +9,39 @@ const emojis = {
   WIN: "🏆",
 };
 const maps = [];
-maps.push(`
-  IXXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  -XXXXXXXXX
-  OXXXXXXXXX
-`);
-maps.push(`
-  O--XXXXXXX
-  X--XXXXXXX
-  XX----XXXX
-  X--XX-XXXX
-  X-XXX--XXX
-  X-XXXX-XXX
-  XX--XX--XX
-  XX--XXX-XX
-  XXXX---IXX
-  XXXXXXXXXX
-  `);
-maps.push(`
-  I-----XXXX
-  XXXXX-XXXX
-  XX----XXXX
-  XX-XXXXXXX
-  XX-----XXX
-  XXXXXX-XXX
-  XX-----XXX
-  XX-XXXXXXX
-  XX-----OXX
-  XXXXXXXXXX
-`);
+maps.push([
+  "IXXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "-XXXXXXXXX",
+  "OXXXXXXXXX",
+]);
+maps.push([
+  "O--XXXXXXX",
+  "X--XXXXXXX",
+  "XX----XXXX",
+  "X--XX-XXXX",
+  "X-XXX--XXX",
+  "X-XXXX-XXX",
+  "XX--XX--XX",
+  "XX--XXX-XX",
+  "XXXX---IXX",
+  "XXXXXXXXXX",
+]);
+maps.push([
+  "I-----XXXX",
+  "XXXXX-XXXX",
+  "XX----XXXX",
+  "XX-XXXXXXX",
+  "XX-----XXX",
+  "XXXXXX-XXX",
+  "XX-----XXX",
+  "XX-XXXXXXX",
+  "XX-----OXX",
+  "XXXXXXXXXX",
+]);
