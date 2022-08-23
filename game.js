2e9d02a0bb6329f0024f0b8f1a0835e6c3d30b1c
AngelVelasco29 <buymatrix@hotmail.com>
2022-08-23 01:25:14 -0500

    Clase 7 Refactor del mapa del juego

--- game.js
+++ game.js
@@ -13,20 +13,33 @@ const startGame = () => {
 
   const elementsSize = canvasSize / 10;
   console.log(canvasSize, elementsSize);
-  game.font = (elementsSize-10) + "px Verdana";
+  game.font = elementsSize - 10 + "px Verdana";
   game.textAlign = "end";
+  // const mapRowCols = mapRows.map((row) => row.split(""));
+  // console.log(mapRows);
+  // console.log(mapRowCols);
 
-
-  const mapRows= maps[2];
-  const mapRowCols= mapRows.map(row => row.split(''));
-  console.log(mapRows);
-  console.log(mapRowCols);
-
-  for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <= 10; col++) {
-      game.fillText(emojis[mapRowCols[row-1][col-1]], elementsSize * col+5, elementsSize * row-10);
+  const mapRows = maps[2];
+  mapRows.forEach((row, rowIndex) => {
+    for (let col = 0; col < row.length; col++) {
+      const emoji = emojis[row[col]];
+      const posX = elementsSize * (col + 1) + 5;
+      const posY = elementsSize * (rowIndex + 1) - 10;
+      game.fillText(emoji, posX, posY);
     }
-  }
+  });
+    // row.forEach((col, colIndex) => {
+    //   const emoji = emojis[col];
+    //   const posX = elementsSize * (colIndex + 1) + 5;
+    //   const posY = elementsSize * (rowIndex + 1) - 10;
+    //   game.fillText(emoji, posX, posY);
+    // });
+
+  // for (let row = 1; row <= 10; row++) {
+  //   for (let col = 1; col <= 10; col++) {
+  //     game.fillText(emojis[mapRowCols[row-1][col-1]], elementsSize * col+5, elementsSize * row-10);
+  //   }
+  // }
 
   // game.fillRect(0, 0, 100, 100);
   // game.clearRect(50,50,50,50);
